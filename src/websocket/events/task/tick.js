8fb2a368197d01499695eef3c08b7dd5f3316287
fjl716 <fjl716@163.com>
2017-02-20 13:18:33 +0800

    修改task，增加retrytask

--- src/websocket/events/task/tick.js
+++ src/websocket/events/task/tick.js
@@ -3,9 +3,19 @@ import TaskTick from './tasktick'
 export default {
   event: 'task.tick',
   invoke: async function (params) {
-    const {equipment} = params;
+    const {angler,equipment} = params;
     equipment.work(new TaskTick(`${Math.random()}`.substr(2), (result) => {
-      console.log(result);
+      angler.send(
+        params,
+        {
+          packet: {
+            event: `tick.complete`,
+            data: result
+          }
+        },
+        true
+      );
+
     }));
   }
 }
