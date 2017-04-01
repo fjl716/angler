61e80cba7728f189ef91711a0629742339db1f20
fjl716 <fjl716@163.com>
2017-04-01 15:13:54 +0800

    修改init的错误

--- src/angler/session.js
+++ src/angler/session.js
@@ -2,11 +2,13 @@ import dbs from './dbs'
 
 export default {
   async get({equipment}){
-    let obj = await dbs.session.findOne('session', {
+    const {session} = dbs.mongoDB;
+
+    let obj = await session.findOne('session', {
       _id: equipment.__ID__
     });
     if (!obj) {
-      obj = await dbs.session.insert('session', {
+      obj = await session.insert('session', {
         _id: equipment.__ID__
       })
     }
@@ -16,18 +18,19 @@ export default {
 
   },
   async set({equipment}, data){
-    let obj = await dbs.session.findOne('session', {
+    const {session} = dbs.mongoDB;
+    let obj = await session.findOne('session', {
       _id: equipment.__ID__
     });
     if (obj) {
-      await dbs.session.update('session', {
+      await session.update('session', {
           _id: equipment.__ID__
         }, {
           '$set': data
         }
       );
     } else {
-      obj = await dbs.session.insert('session', {
+      obj = await session.insert('session', {
         _id: equipment.__ID__,
         data
       })
