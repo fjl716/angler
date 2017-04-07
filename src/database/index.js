c7e0c56ee1f104b95d2377137857f730570ef924
fjl716 <fjl716@163.com>
2017-04-07 14:07:09 +0800

    修改solr

--- src/database/index.js
+++ src/database/index.js
@@ -35,14 +35,14 @@ async function init() {
   // });
   // dbs.solr.user.client.commit();
  // const data = await dbs.solr.user.add({
-  const data = await dbs.solr.user.add({
-    _id: 'a23456789012345678901234',
-    name: 'admin',
-    loginid: 'admin',
-    email: 'fjl716@163.com',
-    sex: 1,
-  });
-  console.log(data);
+ //  const data = await dbs.solr.user.add({
+ //    _id: 'a23456789012345678901234',
+ //    name: 'admin',
+ //    loginid: 'admin',
+ //    email: 'fjl716@163.com',
+ //    sex: 1,
+ //  });
+ //  console.log(data);
 }
 
 export {
