058cdd90b90c3e5b4de5f0de42fa50cbebdd5b6c
fjl716 <fjl716@163.com>
2017-04-12 11:06:58 +0800

    修改框架结构

---
+++ src/angler/loader.js
@@ -0,0 +1,29 @@
+import {MongoDataBase} from './database/mongo';
+import angler,{initContainers,initMongo,initMySql,initSolr,initEvent} from './index'
+
+async function mongoLoader(confDB) {
+
+  const conf = new MongoDataBase(confDB);
+  await conf.init();
+
+  const container = await conf.find('container', {pageSize: 1000});
+  await initContainers(container);
+
+  const mongo = await conf.find('mongo', {pageSize: 1000});
+  await initMongo(mongo);
+
+  const mysql = await conf.find('mysql',{pageSize: 1000});
+  await initMySql(mysql);
+
+  const solr = await conf.find('solr',{pageSize: 1000});
+  await initSolr(solr);
+
+
+  const event = await conf.find('event',{pageSize: 1000});
+  await initEvent(event);
+  return angler;
+}
+
+export {
+  mongoLoader
+}
--- src/server.js
+++ src/server.js
@@ -1,35 +1,8 @@
-import {MongoDataBase} from './angler/database/mongo';
-import angler,{initContainers,initMongo,initMySql,initSolr,initEvent} from './angler'
+import {mongoLoader} from './angler/loader'
 
-
-async function init(confDB) {
-
-  const conf = new MongoDataBase(confDB);
-  await conf.init();
-
-  const container = await conf.find('container', {pageSize: 1000});
-  await initContainers(container);
-
-  const mongo = await conf.find('mongo', {pageSize: 1000});
-  await initMongo(mongo);
-
-  const mysql = await conf.find('mysql',{pageSize: 1000});
-  await initMySql(mysql);
-
-  const solr = await conf.find('solr',{pageSize: 1000});
-  await initSolr(solr);
-
-  const event = await conf.find('event',{pageSize: 1000});
-  await initEvent(event);
-
-  angler.start();
-
-  // let c = Object.values(angler.containers)[0];
-  // console.log(c.session.get());
-  // console.log('start ',Object.keys(angler.containers));
-}
-
-init({
+mongoLoader({
   host: 'localhost',
   database: 'angler'
-}).then();
+}).then((angler)=>{
+  angler.start();
+});
