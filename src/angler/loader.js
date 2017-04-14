d2663723f09e47f77cdaf3224581bc747bde6232
fjl716 <fjl716@163.com>
2017-04-14 17:01:05 +0800

    修改框架结构

--- src/angler/loader.js
+++ src/angler/loader.js
@@ -4,31 +4,51 @@ import log4js from 'log4js'
 
 const logger = log4js.getLogger('angler');
 
+async function init(step,func) {
+  try {
+    await func();
+  }catch (ex){
+    logger.fatal(`initialize ${step} failed`);
+    return true;
+  }
+  logger.trace(`initialize ${step} finish`);
+  return false;
+}
+
 async function mongoLoader(confDB) {
   const conf = new MongoDataBase(confDB);
   await conf.init();
   logger.trace('link confDB');
 
-  const container = await conf.find('container', {pageSize: 1000});
-  await initContainers(container);
-  logger.trace('initialize Containers finish');
+  if (await init('Containers', async () => {
+      const container = await conf.find('container', {pageSize: 1000});
+      await initContainers(container);
+    }))
+    return;
 
-  const mongo = await conf.find('mongo', {pageSize: 1000});
-  await initMongo(mongo);
-  logger.trace('initialize MongoDB finish');
+  if (await init('MongoDB', async () => {
+      const mongo = await conf.find('mongo', {pageSize: 1000});
+      await initMongo(mongo);
+    }))
+    return;
 
-  const mysql = await conf.find('mysql',{pageSize: 1000});
-  logger.trace('initialize MySql finish');
-  await initMySql(mysql);
+  if (await init('MySql', async () => {
+      const mysql = await conf.find('mysql', {pageSize: 1000});
+      await initMySql(mysql);
+    }))
+    return;
 
-  const solr = await conf.find('solr',{pageSize: 1000});
-  await initSolr(solr);
-  logger.trace('initialize Solr finish');
+  if (await init('Solr', async () => {
+      const solr = await conf.find('solr', {pageSize: 1000});
+      await initSolr(solr);
+    }))
+    return;
 
-  const event = await conf.find('event',{pageSize: 1000});
-  await initEvent(event);
-  logger.trace('initialize Event finish');
-
+  if (await init('Event', async () => {
+      const event = await conf.find('event', {pageSize: 1000});
+      await initEvent(event);
+    }))
+    return;
   return angler;
 }
 
